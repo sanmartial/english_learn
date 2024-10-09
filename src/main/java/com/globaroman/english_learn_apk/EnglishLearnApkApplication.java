dd6e97b7b5693ef8e37ba8d00737935cfa6ca9ac
Roman Globa <globaroman@gmail.com>
2024-10-09 19:00:01 +0300

    Creat project

---
+++ src/main/java/com/globaroman/english_learn_apk/EnglishLearnApkApplication.java
@@ -0,0 +1,13 @@
+package com.globaroman.english_learn_apk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishLearnApkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishLearnApkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globaroman/english_learn_apk/EnglishLearnApkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaroman.english_learn_apk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishLearnApkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
